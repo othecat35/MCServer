710cfc1a9ff029990d968bbb7407bb5a32d07381
Othecat35 <othecat35@gmail.com>
2026-01-29 19:10:49 +0700

    Update command_args to use config

--- MCServer.py
+++ MCServer.py
@@ -1,12 +1,29 @@
 import os
 
+config = {
+  "server": {
+    "jarfile": "Launcher.jar",
+    "ram": {
+      "min": "512M",
+      "max": "2G"
+    },
+    "hide_gui": True
+  }
+}
+
+server_config = config["server"]
+server_ram = server_config["ram"]
+
 server_command_args = [
   "java",
-  "-Xms512M",
-  "-Xmx2G",
+  f"-Xms{server_ram["min"]}",
+  f"-Xmx2G{server_ram["max"]}",
   "-jar",
-  "Launcher.jar",
-  "nogui"
+  server_config["jarfile"]
 ]
 
+if server_config["hide_gui"]:
+  server_command_args.append("nogui")
+
+print(f"[INFO]: Running command: {' '.join(server_command_args)}")
 os.execvp("java", server_command_args)
