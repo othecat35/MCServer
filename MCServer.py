4721aba60768027cba79f0199971b06e67cb1ab3
Othecat35 <othecat35@gmail.com>
2026-01-29 19:13:56 +0700

    Refactor the code in a function and run if main

--- MCServer.py
+++ MCServer.py
@@ -11,19 +11,26 @@ config = {
   }
 }
 
-server_config = config["server"]
-server_ram = server_config["ram"]
+def start_server():
+  server_config = config["server"]
+  server_ram = server_config["ram"]
 
-server_command_args = [
-  "java",
-  f"-Xms{server_ram["min"]}",
-  f"-Xmx2G{server_ram["max"]}",
-  "-jar",
-  server_config["jarfile"]
-]
+  server_command_args = [
+    "java",
+    f"-Xms{server_ram["min"]}",
+    f"-Xmx{server_ram["max"]}",
+    "-jar",
+    server_config["jarfile"]
+  ]
 
-if server_config["hide_gui"]:
-  server_command_args.append("nogui")
+  if server_config["hide_gui"]:
+    server_command_args.append("nogui")
 
-print(f"[INFO]: Running command: {' '.join(server_command_args)}")
-os.execvp("java", server_command_args)
+  print(f"[INFO]: Running command: {' '.join(server_command_args)}")
+  os.execvp("java", server_command_args)
+
+def main():
+  start_server()
+
+if __name__ == "__main__":
+  main()
